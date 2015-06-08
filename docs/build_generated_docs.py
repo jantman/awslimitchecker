2ca36365d3db7e14b983ff9a07d75c763533640c
Jason Antman <jason@jasonantman.com>
2015-06-08 10:17:29 -0400

    finish automated build of limits doc

--- docs/build_generated_docs.py
+++ docs/build_generated_docs.py
@@ -84,12 +84,76 @@ def build_iam_policy(checker):
     with open(fname, 'w') as fh:
         fh.write(doc)
 
-"""
-def build_checks(checker):
+def build_limits(checker):
+    logger.info("Beginning build of limits.rst")
+    # get the policy dict
+    logger.info("Getting Limits")
+    ta_info = 'TODO'  # @TODO trusted advisor data
+    limit_info = ''
     limits = checker.get_limits()
-    for svc_name in limits:
-        for limit in limits[svc_name]:
-"""         
+    # this is a bit of a pain, because we need to know string lengths to build the table
+    for svc_name in sorted(limits):
+        limit_info += svc_name + "\n"
+        limit_info += ('+' * (len(svc_name)+1)) + "\n"
+        limit_info += "\n"
+        # build a dict of the limits
+        slimits = {}
+        # track the maximum string lengths
+        max_name = 0
+        max_default_limit = 0
+        for limit in limits[svc_name].values():
+            # update max string length for table formatting
+            if len(limit.name) > max_name:
+                max_name = len(limit.name)
+            if len(str(limit.default_limit)) > max_default_limit:
+                max_default_limit = len(str(limit.default_limit))
+            # @TODO trusted advisor, use :sup:`[TA]`
+            slimits[limit.name] = str(limit.default_limit)
+        # create the format string
+        sformat = '{name: <' + str(max_name) + '} ' \
+                  '{limit: <' + str(max_default_limit) + '}\n'
+        # separator lines
+        sep = ('=' * max_name) + ' ' + ('=' * max_default_limit) + "\n"
+        # header
+        limit_info += sep
+        limit_info += sformat.format(name='Limit', limit='Default')
+        limit_info += sep
+        # limit lines
+        for lname, limit in sorted(slimits.iteritems()):
+            limit_info += sformat.format(name=lname, limit=limit)
+        # footer
+        limit_info += sep
+
+    doc = """
+    .. _limits:
+
+    Supported Limits
+    ================
+
+    .. _limits.trusted_advisor:
+
+    Trusted Advisor Data
+    ---------------------
+
+    {ta_info}
+
+    .. _limits.checks:
+
+    Current Checks
+    ---------------
+
+    The section below lists every limit that this version of awslimitchecker knows
+    how to check, and its hard-coded default value (per AWS documentation).
+
+    {limit_info}
+
+    """
+    doc = dedent(doc)
+    doc = doc.format(ta_info=ta_info, limit_info=limit_info)
+    fname = os.path.join(my_dir, 'source', 'limits.rst')
+    logger.info("Writing {f}".format(f=fname))
+    with open(fname, 'w') as fh:
+        fh.write(doc)
 
 def build_docs():
     """
@@ -100,7 +164,7 @@ def build_docs():
     logger.info("Instantiating AwsLimitChecker")
     c = AwsLimitChecker()
     build_iam_policy(c)
-    #build_checks(c)
+    build_limits(c)
 
 if __name__ == "__main__":
     build_docs()
