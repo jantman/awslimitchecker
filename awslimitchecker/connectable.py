jantman/awslimitchecker

commit c80eab75401f807318c0991a8a533a0e0d583376

issue #95 - convert TrustedAdvisor to use boto3

awslimitchecker/connectable.py
@@ -107,7 +107,7 @@ class Connectable(object):
         logger.info("Connected to %s", self.service_name)
         return conn
 
-    def connect_client(self, service_name):
+    def connect_client(self, service_name, region=None):
         """
         Connect to an AWS API and return the connected boto3 client object. If
         ``self.account_id`` is None, call :py:meth:`boto3.client` with
@@ -121,26 +121,31 @@ class Connectable(object):
         :param service_name: name of the AWS service API to connect to (passed
           to :py:meth:`boto3.client` as the ``service_name`` parameter.)
         :type driver: str
+        :param region: the region name to connect to.
+          if None, use ``self.region``
+        :type region_attr: str
         :returns: connected :py:meth:`boto3.client` class instance
         """
+        if region is None:
+            region = self.region
         if self.account_id is not None:
             if Connectable.credentials is None:
                 logger.debug("Connecting to %s for account %s (STS; %s)",
-                             service_name, self.account_id, self.region)
+                             service_name, self.account_id, region)
                 Connectable.credentials = self._get_sts_token_boto3()
             else:
                 logger.debug("Reusing previous STS credentials for account %s",
                              self.account_id)
             conn = boto3.client(
                 service_name,
-                region_name=self.region,
+                region_name=region,
                 aws_access_key_id=Connectable.credentials.access_key,
                 aws_secret_access_key=Connectable.credentials.secret_key,
                 aws_session_token=Connectable.credentials.session_token)
         else:
             logger.debug("Connecting to %s (%s)",
-                         service_name, self.region)
-            conn = boto3.client(service_name, region_name=self.region)
+                         service_name, region)
+            conn = boto3.client(service_name, region_name=region)
         logger.info("Connected to %s", service_name)
         return conn
 

awslimitchecker/trustedadvisor.py
@@ -37,8 +37,7 @@ Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 ################################################################################
 """
 
-import boto
-import boto.support
+from botocore.exceptions import ClientError
 from dateutil import parser
 import logging
 from .connectable import Connectable
@@ -98,14 +97,8 @@ class TrustedAdvisor(Connectable):
         """Connect to API if not already connected; set self.conn."""
         if self.conn is not None:
             return
-        if self.ta_region:
-            logger.debug("Connecting to Support API (TrustedAdvisor) in %s",
-                         self.region)
-            self.conn = self.connect_via(boto.support.connect_to_region)
         else:
-            logger.debug("Connecting to Support API (TrustedAdvisor)")
-            self.conn = boto.connect_support()
-        logger.debug("Connected to Support API")
+            self.conn = self.connect_client('support', region=self.ta_region)
 
     def update_limits(self, services):
         """
@@ -148,10 +141,10 @@ class TrustedAdvisor(Connectable):
                             "check; not using Trusted Advisor data.")
             return
         check_id, metadata = tmp
-        region = self.ta_region or self.conn.region.name
+        region = self.ta_region or self.conn._client_config.region_name
         checks = boto_query_wrapper(
             self.conn.describe_trusted_advisor_check_result,
-            check_id, alc_no_paginate=True
+            checkId=check_id, language='en', alc_no_paginate=True
         )
         check_datetime = parser.parse(checks['result']['timestamp'])
         logger.debug("Got TrustedAdvisor data for check %s as of %s",
@@ -180,13 +173,10 @@ class TrustedAdvisor(Connectable):
         try:
             checks = boto_query_wrapper(
                 self.conn.describe_trusted_advisor_checks,
-                'en', alc_no_paginate=True
+                language='en', alc_no_paginate=True
             )['checks']
-        except boto.exception.JSONResponseError as ex:
-            if (
-                    '__type' in ex.body and
-                    ex.body['__type'] == 'SubscriptionRequiredException'
-            ):
+        except ClientError as ex:
+            if ex.response['Error']['Code'] == 'SubscriptionRequiredException':
                 logger.warning(
                     "Cannot check TrustedAdvisor: %s",
                     ex.message

awslimitchecker/tests/test_connectable.py
@@ -150,6 +150,15 @@ class Test_Connectable(object):
         ]
         assert res == mock_client.return_value
 
+    def test_connect_client_region_arg(self):
+        cls = ConnectableTester()
+        with patch('%s.boto3.client' % pbm) as mock_client:
+            res = cls.connect_client('foo', region='bar')
+        assert mock_client.mock_calls == [
+            call('foo', region_name='bar')
+        ]
+        assert res == mock_client.return_value
+
     def test_connect_client_with_region(self):
         cls = ConnectableTester(region='myregion')
         with patch('%s._get_sts_token_boto3' % pb) as mock_get_sts:

awslimitchecker/tests/test_trustedadvisor.py
@@ -38,10 +38,7 @@ Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 """
 
 import sys
-from boto.support.layer1 import SupportConnection
-from boto.support import connect_to_region
-from boto.regioninfo import RegionInfo
-from boto.exception import JSONResponseError, BotoServerError
+from botocore.exceptions import ClientError
 from awslimitchecker.trustedadvisor import TrustedAdvisor
 from awslimitchecker.services.base import _AwsService
 import pytest
@@ -64,8 +61,10 @@ pb = '%s.TrustedAdvisor' % pbm
 class Test_TrustedAdvisor(object):
 
     def setup(self):
-        self.mock_conn = Mock(spec_set=SupportConnection)
-        type(self.mock_conn).region = RegionInfo(name='us-east-1')
+        self.mock_conn = Mock()
+        self.mock_client_config = Mock()
+        type(self.mock_client_config).region_name = 'us-east-1'
+        type(self.mock_conn)._client_config = self.mock_client_config
         self.cls = TrustedAdvisor()
         self.cls.conn = self.mock_conn
 
@@ -111,43 +110,42 @@ class Test_TrustedAdvisor(object):
         assert cls.mfa_token is None
 
     def test_connect(self):
-        cls = TrustedAdvisor()
-        mock_conn = Mock(spec_set=SupportConnection, name='mock_conn')
-        with patch('awslimitchecker.trustedadvisor.boto.connect_support'
-                   '', autospec=True) as mock_connect:
-            mock_connect.return_value = mock_conn
-            cls.connect()
+        """test connect()"""
+        mock_conn = Mock()
+        cls = TrustedAdvisor(21, 43)
+        with patch('%s.connect_client' % pb) as mock_connect_client:
+                mock_connect_client.return_value = mock_conn
+                cls.connect()
+        assert mock_conn.mock_calls == []
+        assert mock_connect_client.mock_calls == [
+            call('support', region=None)
+        ]
         assert cls.conn == mock_conn
-        assert mock_connect.mock_calls == [call()]
 
     def test_connect_region(self):
-        cls = TrustedAdvisor(account_id='foo', account_role='bar', region='re')
-        mock_conn = Mock(spec_set=SupportConnection, name='mock_conn')
-        mock_conn_via = Mock(spec_set=SupportConnection, name='mock_conn')
-        with patch('awslimitchecker.trustedadvisor.TrustedAdvisor.connect_via'
-                   '') as mock_connect_via:
-            mock_connect_via.return_value = mock_conn_via
-            with patch('awslimitchecker.trustedadvisor.boto.connect_support'
-                       '', autospec=True) as mock_connect:
-                mock_connect.return_value = mock_conn
+        """test connect()"""
+        mock_conn = Mock()
+        cls = TrustedAdvisor(21, 43)
+        cls.ta_region = 'foo'
+        with patch('%s.connect_client' % pb) as mock_connect_client:
+                mock_connect_client.return_value = mock_conn
                 cls.connect()
-        assert cls.conn == mock_conn_via
-        assert mock_connect.mock_calls == []
-        assert mock_connect_via.mock_calls == [
-            call(connect_to_region)
+        assert mock_conn.mock_calls == []
+        assert mock_connect_client.mock_calls == [
+            call('support', region='foo')
         ]
+        assert cls.conn == mock_conn
 
     def test_connect_again(self):
-        cls = TrustedAdvisor()
-        mock_original_conn = Mock(spec_set=SupportConnection)
-        cls.conn = mock_original_conn
-        mock_conn = Mock(spec_set=SupportConnection)
-        with patch('awslimitchecker.trustedadvisor.boto.connect_support'
-                   '') as mock_connect:
-            mock_connect.return_value = mock_conn
-            cls.connect()
-        assert cls.conn == mock_original_conn
-        assert mock_connect.mock_calls == []
+        """make sure we re-use the connection"""
+        mock_conn = Mock()
+        cls = TrustedAdvisor(21, 43)
+        cls.conn = mock_conn
+        with patch('%s.connect_client' % pb) as mock_connect_client:
+                mock_connect_client.return_value = mock_conn
+                cls.connect()
+        assert mock_conn.mock_calls == []
+        assert mock_connect_client.mock_calls == []
 
     def test_update_limits(self):
         mock_results = Mock()
@@ -203,7 +201,7 @@ class Test_TrustedAdvisor(object):
         assert self.mock_conn.mock_calls == []
         assert mock_wrapper.mock_calls == [call(
             self.mock_conn.describe_trusted_advisor_checks,
-            'en', alc_no_paginate=True
+            language='en', alc_no_paginate=True
             )]
 
     def test_get_limit_check_id_none(self):
@@ -228,20 +226,24 @@ class Test_TrustedAdvisor(object):
         assert self.mock_conn.mock_calls == []
         assert mock_wrapper.mock_calls == [call(
             self.mock_conn.describe_trusted_advisor_checks,
-            'en', alc_no_paginate=True
+            language='en', alc_no_paginate=True
         )]
 
     def test_get_limit_check_id_subscription_required(self):
 
         def se_api(foo, language, alc_no_paginate=False):
-            status = 400
-            reason = 'Bad Request'
-            body = {
-                'message': 'AWS Premium Support Subscription is required to '
-                'use this service.',
-                '__type': 'SubscriptionRequiredException'
+            response = {
+                'ResponseMetadata': {
+                    'HTTPStatusCode': 400,
+                    'RequestId': '3cc9b2a8-c6e5-11e5-bc1d-b13dcea36176'
+                },
+                'Error': {
+                    'Message': 'AWS Premium Support Subscription is required '
+                               'to use this service.',
+                    'Code': 'SubscriptionRequiredException'
+                }
             }
-            raise JSONResponseError(status, reason, body)
+            raise ClientError(response, 'operation')
 
         assert self.cls.have_ta is True
         with patch('awslimitchecker.trustedadvisor'
@@ -254,38 +256,45 @@ class Test_TrustedAdvisor(object):
         assert self.mock_conn.mock_calls == []
         assert mock_wrapper.mock_calls == [call(
             self.mock_conn.describe_trusted_advisor_checks,
-            'en', alc_no_paginate=True
+            language='en', alc_no_paginate=True
         )]
         assert mock_logger.mock_calls == [
             call.debug("Querying Trusted Advisor checks"),
             call.warning("Cannot check TrustedAdvisor: %s",
-                         "AWS Premium Support "
-                         "Subscription is required to use this service.")
+                         'An error occurred (SubscriptionRequiredException) '
+                         'when calling the operation operation: AWS Premium '
+                         'Support Subscription is required to use this '
+                         'service.')
         ]
 
     def test_get_limit_check_id_other_exception(self):
 
         def se_api(foo, language, alc_no_paginate=False):
-            status = 400
-            reason = 'foobar'
-            body = {
-                'message': 'other message',
-                '__type': 'OtherException'
+            response = {
+                'ResponseMetadata': {
+                    'HTTPStatusCode': 400,
+                    'RequestId': '3cc9b2a8-c6e5-11e5-bc1d-b13dcea36176'
+                },
+                'Error': {
+                    'Message': 'foo',
+                    'Code': 'SomeOtherException'
+                }
             }
-            raise JSONResponseError(status, reason, body)
+            raise ClientError(response, 'operation')
 
-        with pytest.raises(BotoServerError) as excinfo:
+        with pytest.raises(ClientError) as excinfo:
             with patch('%s.boto_query_wrapper' % pbm) as mock_wrapper:
                 mock_wrapper.side_effect = se_api
                 self.cls._get_limit_check_id()
         assert self.mock_conn.mock_calls == []
         assert mock_wrapper.mock_calls == [call(
             self.mock_conn.describe_trusted_advisor_checks,
-            'en', alc_no_paginate=True
+            language='en', alc_no_paginate=True
         )]
-        assert excinfo.value.status == 400
-        assert excinfo.value.reason == 'foobar'
-        assert excinfo.value.body['__type'] == 'OtherException'
+        assert excinfo.value.response['ResponseMetadata'][
+                   'HTTPStatusCode'] == 400
+        assert excinfo.value.response['Error']['Message'] == 'foo'
+        assert excinfo.value.response['Error']['Code'] == 'SomeOtherException'
 
     def test_poll_id_none(self):
         tmp = self.mock_conn.describe_trusted_advisor_check_result
@@ -365,7 +374,7 @@ class Test_TrustedAdvisor(object):
                 res = self.cls._poll()
         assert tmp.mock_calls == []
         assert mock_wrapper.mock_calls == [
-            call(tmp, 'foo', alc_no_paginate=True)
+            call(tmp, checkId='foo', language='en', alc_no_paginate=True)
         ]
         assert mock_id.mock_calls == [call(self.cls)]
         assert res == {
@@ -444,7 +453,7 @@ class Test_TrustedAdvisor(object):
                 res = self.cls._poll()
         assert tmp.mock_calls == []
         assert mock_wrapper.mock_calls == [
-            call(tmp, 'foo', alc_no_paginate=True)
+            call(tmp, checkId='foo', language='en', alc_no_paginate=True)
         ]
         assert mock_id.mock_calls == [call(self.cls)]
         assert res == {

commit b87b3cd448b46aecb4cdc295d601a90595d84005

fix integration test regression caused by c80eab75401f807318c0991a8a533a0e0d583376

awslimitchecker/connectable.py
@@ -108,7 +108,7 @@ class Connectable(object):
         logger.info("Connected to %s", self.service_name)
         return conn
 
-    def connect_client(self, service_name, region=None):
+    def connect_client(self, service_name):
         """
         Connect to an AWS API and return the connected boto3 client object. If
         ``self.account_id`` is None, call `boto3.client <https://boto3.readthed
@@ -126,31 +126,26 @@ class Connectable(object):
         :param service_name: name of the AWS service API to connect to (passed
           to ``boto3.client`` as the ``service_name`` parameter.)
         :type driver: str
-        :param region: the region name to connect to.
-          if None, use ``self.region``
-        :type region_attr: str
         :returns: connected ``boto3.client`` class instance
         """
-        if region is None:
-            region = self.region
         if self.account_id is not None:
             if Connectable.credentials is None:
                 logger.debug("Connecting to %s for account %s (STS; %s)",
-                             service_name, self.account_id, region)
+                             service_name, self.account_id, self.region)
                 Connectable.credentials = self._get_sts_token_boto3()
             else:
                 logger.debug("Reusing previous STS credentials for account %s",
                              self.account_id)
             conn = boto3.client(
                 service_name,
-                region_name=region,
+                region_name=self.region,
                 aws_access_key_id=Connectable.credentials.access_key,
                 aws_secret_access_key=Connectable.credentials.secret_key,
                 aws_session_token=Connectable.credentials.session_token)
         else:
             logger.debug("Connecting to %s (%s)",
-                         service_name, region)
-            conn = boto3.client(service_name, region_name=region)
+                         service_name, self.region)
+            conn = boto3.client(service_name, region_name=self.region)
         logger.info("Connected to %s", service_name)
         return conn
 

awslimitchecker/trustedadvisor.py
@@ -98,7 +98,7 @@ class TrustedAdvisor(Connectable):
         if self.conn is not None:
             return
         else:
-            self.conn = self.connect_client('support', region=self.ta_region)
+            self.conn = self.connect_client('support')
 
     def update_limits(self, services):
         """

awslimitchecker/tests/test_connectable.py
@@ -141,21 +141,12 @@ class Test_Connectable(object):
         ]
         assert res == mock_driver.return_value
 
-    def test_connect_client_no_region(self):
+    def test_connect_client(self):
         cls = ConnectableTester()
         with patch('%s.boto3.client' % pbm) as mock_client:
             res = cls.connect_client('foo')
         assert mock_client.mock_calls == [
             call('foo', region_name=None)
-        ]
-        assert res == mock_client.return_value
-
-    def test_connect_client_region_arg(self):
-        cls = ConnectableTester()
-        with patch('%s.boto3.client' % pbm) as mock_client:
-            res = cls.connect_client('foo', region='bar')
-        assert mock_client.mock_calls == [
-            call('foo', region_name='bar')
         ]
         assert res == mock_client.return_value
 

awslimitchecker/tests/test_trustedadvisor.py
@@ -118,7 +118,7 @@ class Test_TrustedAdvisor(object):
                 cls.connect()
         assert mock_conn.mock_calls == []
         assert mock_connect_client.mock_calls == [
-            call('support', region=None)
+            call('support')
         ]
         assert cls.conn == mock_conn
 
@@ -132,7 +132,7 @@ class Test_TrustedAdvisor(object):
                 cls.connect()
         assert mock_conn.mock_calls == []
         assert mock_connect_client.mock_calls == [
-            call('support', region='foo')
+            call('support')
         ]
         assert cls.conn == mock_conn
 