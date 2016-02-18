ff74edc0a798bf136ca72ea3d5df7a7a1790b842
Jason Antman <jason@jasonantman.com>
2016-02-18 06:42:47 -0500

    ignore codecov.io from linkcheck as well

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -295,8 +295,9 @@ nitpick_ignore = [
 
 linkcheck_ignore = [
     r'https?://landscape\.io.*',
-    r'https?://www.virtualenv.org.*',
-    r'https?://boto3.readthedocs.org.*'
+    r'https?://www\.virtualenv\.org.*',
+    r'https?://.*\.readthedocs\.org.*',
+    r'https?://codecov\.io.*'
 ]
 
 # exclude module docstrings - see http://stackoverflow.com/a/18031024/211734
