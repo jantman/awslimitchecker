344561756ce79a96d3f7d6a68984d4c4c9e4e37f
Di Zou <zou@pythian.com>
2018-01-15 09:11:09 -0500

    fix some lint issues 1

--- awslimitchecker/services/ecs.py
+++ awslimitchecker/services/ecs.py
@@ -64,7 +64,7 @@ class _EcsService(_AwsService):
         self._find_usage_clusters()
         self._have_usage = True
         logger.debug("Done checking usage.")
-    
+
     def _find_usage_clusters(self):
         count = 0
         paginator = self.conn.get_paginator('list_clusters')
@@ -73,9 +73,7 @@ class _EcsService(_AwsService):
             for clusterArn in page['clusterArns']:
                 count += 1
                 resp = self.conn.describe_clusters(
-                    clusters = [
-                        clusterArn,
-                    ]
+                    clusters = [clusterArn,]
                 )
                 cluster = resp['clusters'][0]
                 self.limits['Container Instances per Cluster']._add_current_usage(
