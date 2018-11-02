50ca34b6cb9ca22b550e01eb8062cf509778033a
Ronnie Kilsbo <ronnie@kilsbo.com>
2018-11-02 20:52:42 +0000

    Lambda service added.

---
+++ awslimitchecker/services/lambdafunc.py
@@ -0,0 +1,130 @@
+"""
+awslimitchecker/services/lambdafunc.py
+
+The latest version of this package is available at:
+<https://github.com/jantman/awslimitchecker>
+
+################################################################################
+Copyright 2015-2018 Jason Antman <jason@jasonantman.com>
+
+    This file is part of awslimitchecker, also known as awslimitchecker.
+
+    awslimitchecker is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    awslimitchecker is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with awslimitchecker.  If not, see <http://www.gnu.org/licenses/>.
+
+The Copyright and Authors attributions contained herein may not be removed or
+otherwise altered, except to add the Author attribution of a contributor to
+this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+################################################################################
+While not legally required, I sincerely request that anyone who finds
+bugs please submit them at <https://github.com/jantman/awslimitchecker> or
+to me via email, and that you send any contributions or improvements
+either as a pull request on GitHub, or to me via email.
+################################################################################
+
+AUTHORS:
+Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+################################################################################
+"""
+
+import abc  # noqa
+import logging
+
+from .base import _AwsService
+from ..limit import AwsLimit
+
+logger = logging.getLogger(__name__)
+
+
+class _LambdaService(_AwsService):
+
+    service_name = 'Lambda'
+    api_name = 'lambda'  # AWS API name to connect to (boto3.client)
+
+    def find_usage(self):
+        """
+        Determine the current usage for each limit of this service,
+        and update corresponding Limit via
+        :py:meth:`~.AwsLimit._add_current_usage`.
+        """
+        logger.debug("Checking usage for service %s", self.service_name)
+        self.connect()
+        for lim in self.limits.values():
+            lim._reset_usage()
+        # TODO: update your usage here, i.e.:
+        """
+        usage = self.conn.method_to_get_usage()
+        # or, if it needs to be paginated,  something like:
+        # remebering to 'from ..utils import paginate_dict'
+        usage = paginate_dict(
+            self.conn.method_to_get_usage,
+            alc_marker_path=['NextToken'],
+            alc_data_path=['ResourceListName'],
+            alc_marker_param='NextToken'
+        )
+        u_id = (resource id from AWS)
+        self.limits['Number of u']._add_current_usage(u, aws_type='U', id=u_id)
+        """
+        self._have_usage = True
+        logger.debug("Done checking usage.")
+
+    def get_limits(self):
+        """
+        Return all known limits for this service, as a dict of their names
+        to :py:class:`~.AwsLimit` objects.
+
+        :returns: dict of limit names to :py:class:`~.AwsLimit` objects
+        :rtype: dict
+        """
+        if self.limits != {}:
+            return self.limits
+        limits = {}
+        # TODO: declare Limits here, i.e.:
+        """
+        limits['Number of u'] = AwsLimit(
+            'Number of u',
+            self,
+            40000,
+            self.warning_threshold,
+            self.critical_threshold,
+            limit_type='U',
+        )
+
+        """
+        self.limits = limits
+        return limits
+
+    def _update_limits_from_api(self):
+        """
+        Call the service's API action to retrieve limit/quota information, and
+        update AwsLimit objects in ``self.limits`` with this information.
+        """
+        # TODO: if the service has an API that can retrieve current limit information
+        # i.e. ``DescribeAccountAttributes``, call that action here and update each
+        # relevant AwsLimit object (in ``self.limits``) via its ``._set_api_limit()`` method.
+        # ELSE if the service has no API call for this, remove this method.
+        raise NotImplementedException()
+
+    def required_iam_permissions(self):
+        """
+        Return a list of IAM Actions required for this Service to function
+        properly. All Actions will be shown with an Effect of "Allow"
+        and a Resource of "*".
+
+        :returns: list of IAM Action strings
+        :rtype: list
+        """
+        # TODO: update this to be all IAM permissions required for find_usage() to work
+        return [
+            "Lambda:SomeAction",
+        ]
---
+++ awslimitchecker/tests/services/test_lambdafunc.py
@@ -0,0 +1,110 @@
+"""
+awslimitchecker/tests/services/test_lambdafunc.py
+
+The latest version of this package is available at:
+<https://github.com/jantman/awslimitchecker>
+
+################################################################################
+Copyright 2015-2018 Jason Antman <jason@jasonantman.com>
+
+    This file is part of awslimitchecker, also known as awslimitchecker.
+
+    awslimitchecker is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    awslimitchecker is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with awslimitchecker.  If not, see <http://www.gnu.org/licenses/>.
+
+The Copyright and Authors attributions contained herein may not be removed or
+otherwise altered, except to add the Author attribution of a contributor to
+this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+################################################################################
+While not legally required, I sincerely request that anyone who finds
+bugs please submit them at <https://github.com/jantman/awslimitchecker> or
+to me via email, and that you send any contributions or improvements
+either as a pull request on GitHub, or to me via email.
+################################################################################
+
+AUTHORS:
+Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+################################################################################
+"""
+
+import sys
+from awslimitchecker.tests.services import result_fixtures
+from awslimitchecker.services.lambdafunc import _LambdafuncService
+
+# https://code.google.com/p/mock/issues/detail?id=249
+# py>=3.4 should use unittest.mock not the mock package on pypi
+if (
+        sys.version_info[0] < 3 or
+        sys.version_info[0] == 3 and sys.version_info[1] < 4
+):
+    from mock import patch, call, Mock
+else:
+    from unittest.mock import patch, call, Mock
+
+
+pbm = 'awslimitchecker.services.lambdafunc'  # module patch base
+pb = '%s._LambdafuncService' % pbm  # class patch pase
+
+
+class Test_LambdafuncService(object):
+
+    def test_init(self):
+        """test __init__()"""
+        cls = _LambdafuncService(21, 43)
+        assert cls.service_name == 'Lambdafunc'
+        assert cls.api_name == 'lambdafunc'
+        assert cls.conn is None
+        assert cls.warning_threshold == 21
+        assert cls.critical_threshold == 43
+
+    def test_get_limits(self):
+        cls = _LambdafuncService(21, 43)
+        cls.limits = {}
+        res = cls.get_limits()
+        assert sorted(res.keys()) == sorted([
+            # TODO fill in limits here
+            'SomeLimitNameHere',
+        ])
+        for name, limit in res.items():
+            assert limit.service == cls
+            assert limit.def_warning_threshold == 21
+            assert limit.def_critical_threshold == 43
+
+    def test_get_limits_again(self):
+        """test that existing limits dict is returned on subsequent calls"""
+        mock_limits = Mock()
+        cls = _LambdafuncService(21, 43)
+        cls.limits = mock_limits
+        res = cls.get_limits()
+        assert res == mock_limits
+
+    def test_find_usage(self):
+        # put boto3 responses in response_fixtures.py, then do something like:
+        # response = result_fixtures.EBS.test_find_usage_ebs
+        mock_conn = Mock()
+        mock_conn.some_method.return_value =  # some logical return value
+        with patch('%s.connect' % pb) as mock_connect:
+            cls = _LambdafuncService(21, 43)
+            cls.conn = mock_conn
+            assert cls._have_usage is False
+            cls.find_usage()
+        assert mock_connect.mock_calls == [call()]
+        assert cls._have_usage is True
+        assert mock_conn.mock_calls == [call.some_method()]
+        # TODO - assert about usage
+
+    def test_required_iam_permissions(self):
+        cls = _LambdafuncService(21, 43)
+        assert cls.required_iam_permissions() == [
+            # TODO - permissions here
+        ]
