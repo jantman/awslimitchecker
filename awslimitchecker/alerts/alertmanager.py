9be6738fd57c0c6c68866182b572ca145fd7dcb4
Jason Antman <jason@jasonantman.com>
2020-09-22 11:29:50 -0400

    PR #476 - AlertManager - reformat code and remove f-strings for py35 compatibility

--- awslimitchecker/alerts/alertmanager.py
+++ awslimitchecker/alerts/alertmanager.py
@@ -1,5 +1,40 @@
 """
 awslimitchecker/alerts/alertmanager.py
+
+The latest version of this package is available at:
+<https://github.com/jantman/awslimitchecker>
+
+################################################################################
+Copyright 2015-2019 Jason Antman <jason@jasonantman.com>
+
+    This file is part of awslimitchecker, also known as awslimitchecker.
+
+    awslimitchecker is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    awslimitchecker is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with awslimitchecker.  If not, see <http://www.gnu.org/licenses/>.
+
+The Copyright and Authors attributions contained herein may not be removed or
+otherwise altered, except to add the Author attribution of a contributor to
+this work. (Additional Terms pursuant to Section 7b of the AGPL v3)
+################################################################################
+While not legally required, I sincerely request that anyone who finds
+bugs please submit them at <https://github.com/jantman/awslimitchecker> or
+to me via email, and that you send any contributions or improvements
+either as a pull request on GitHub, or to me via email.
+################################################################################
+
+AUTHORS:
+Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+################################################################################
 """
 import os
 import logging
@@ -19,7 +54,8 @@ class AlertManager(AlertProvider):
     """
 
     def __init__(
-        self, region_name: str, endpoints: str, alert_duration=1800, basic_auth_username: str = None, basic_auth_password: str = None
+        self, region_name: str, endpoints: str, alert_duration=1800,
+        basic_auth_username: str = None, basic_auth_password: str = None
     ):
         """
         Initialize AlertManager alert provider.
@@ -30,9 +66,9 @@ class AlertManager(AlertProvider):
           (ex. endpoints=http://alertmanager1:9093,http://alertmanager2:9093)
           env: ``ALERTMANAGER_ENDPOINTS``
         :type endpoints: str
-        :param alert_duration: **Optional**; seconds before AlertManager considers
-          alerts are resolved. Set it more then period between the program runs.
-          env: ALERTMANAGER_ALERT_DURATION
+        :param alert_duration: **Optional**; seconds before AlertManager
+          considers alerts are resolved. Set it more then period between the
+          program runs. env: ALERTMANAGER_ALERT_DURATION
         :type alert_duration: int
         :param basic_auth_username: **Optional**; Can also be specified via the
           ``ALERTMANAGER_BASIC_AUTH_USERNAME`` environment variable.
@@ -50,9 +86,15 @@ class AlertManager(AlertProvider):
                 'endpoints parameter or ALERTMANAGER_ENDPOINTS '
                 'environment variable.'
             )
-        self._alert_duration = os.environ.get('ALERTMANAGER_ALERT_DURATION') or alert_duration
-        self._basic_auth_username = os.environ.get('ALERTMANAGER_BASIC_AUTH_USERNAME')or basic_auth_username
-        self._basic_auth_password = os.environ.get('ALERTMANAGER_BASIC_AUTH_PASSWORD') or basic_auth_password
+        self._alert_duration = os.environ.get(
+            'ALERTMANAGER_ALERT_DURATION'
+        ) or alert_duration
+        self._basic_auth_username = os.environ.get(
+            'ALERTMANAGER_BASIC_AUTH_USERNAME'
+        ) or basic_auth_username
+        self._basic_auth_password = os.environ.get(
+            'ALERTMANAGER_BASIC_AUTH_PASSWORD'
+        ) or basic_auth_password
 
     def _send_event(self, severity: str, description: str):
         """
@@ -63,9 +105,9 @@ class AlertManager(AlertProvider):
         :param description: event description
         :type description: str
         """
-        # region in the label 'instance' due to currently awslimitchecker supports
-        # only check per region and grouping by instance is default behavior
-        # in alertmanager.
+        # region in the label 'instance' due to currently awslimitchecker
+        # supports only check per region and grouping by instance is default
+        # behavior in alertmanager.
         # datetime in UTC format to simplify the code
         event = '''[{
                     "labels": {
@@ -77,10 +119,14 @@ class AlertManager(AlertProvider):
                        "description": "%s"
                     },
                     "endsAt": "%s"
-                }]''' % (self._region_name,
-                         severity,
-                         description,
-                         (datetime.utcnow() + timedelta(seconds=self._alert_duration)).isoformat() + 'Z')
+                }]''' % (
+            self._region_name,
+            severity,
+            description,
+            (
+                datetime.utcnow() + timedelta(seconds=self._alert_duration)
+            ).isoformat() + 'Z'
+        )
 
         http = urllib3.PoolManager()
         success = 0
@@ -90,24 +136,37 @@ class AlertManager(AlertProvider):
                 'POSTing to AlertManager API (%s): %s', url, event
             )
             try:
+                auth = '%s:%s' % (
+                    self._basic_auth_username, self._basic_auth_password
+                )
                 resp = http.request(
                     'POST', url,
                     headers={
                         'Content-type': 'application/json',
-                        'Authorization': 'Basic %s' % b64encode(f'{self._basic_auth_username}:{self._basic_auth_password}'.encode()).decode()
+                        'Authorization': 'Basic %s' % b64encode(
+                            auth.encode()
+                        ).decode()
                     },
                     body=event
                 )
                 if resp.status == 200:
-                    logger.debug(f'Successfully POSTed to {url}: {resp.status} {resp.data}')
+                    logger.debug(
+                        'Successfully POSTed to %s: %s %s',
+                        url, resp.status, resp.data
+                    )
                     success += 1
                 else:
-                    logger.error(f'Unsuccessfully POSTed to {url}: {resp.status} {resp.data}')
+                    logger.error(
+                        'Unsuccessfully POSTed to %s: %s %s',
+                        url, resp.status, resp.data
+                    )
             except urllib3.exceptions.MaxRetryError as e:
                 logger.error(e)
 
         if success == 0:
-            raise RuntimeError(f'Alert was not able to be sent to {self._endpoints}')
+            raise RuntimeError(
+                'Alert was not able to be sent to %s' % self._endpoints
+            )
 
     def _generate_description(self, problems) -> str:
         """
