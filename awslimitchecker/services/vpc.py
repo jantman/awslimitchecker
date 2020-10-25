1aefc0a06b6ae3bb4e49066fb5bd80ae0938c8cf
Reshad Patuck <reshadpatuck1@gmail.com>
2020-10-25 17:16:42 +0530

    fix(vpc.py): quotas_name not quota_name!

--- awslimitchecker/services/vpc.py
+++ awslimitchecker/services/vpc.py
@@ -309,7 +309,7 @@ class _VpcService(_AwsService):
             self.critical_threshold,
             limit_type='AWS::EC2::Route',
             limit_subtype='AWS::EC2::RouteTable',
-            quota_name='Routes per route table'
+            quotas_name='Routes per route table'
         )
 
         limits['Internet gateways'] = AwsLimit(
